0e905bc063ba6898988b05464f678cba9b0d8ff8
Medina <104513058+018medina@users.noreply.github.com>
2025-01-22 00:24:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event){
+    if(event.type === 'touchstart') event.preventDefault();
+    var nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
