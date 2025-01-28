e48d18f8fa2384bb8bbbfdc2912057d25ff2df55
Medina <104513058+018medina@users.noreply.github.com>
2025-01-28 13:29:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+$(document).ready(function() {
+    // Seleciona todos os cards
+    const cards = document.querySelectorAll('.cardT');
+
+    cards.forEach(function(card) {
+    // Dentro de cada card, seleciona a descrição
+    const description = card.querySelector('.descricaoAreas');
+
+    // Evento para mostrar a descrição quando o mouse passar por cima do card
+    card.addEventListener('mouseover', function() {
+        description.style.display = 'flex'; // Torna a descrição visível
+    });
+
+    // Evento para esconder a descrição quando o mouse sair do card
+    card.addEventListener('mouseout', function() {
+        description.style.display = 'none'; // Torna a descrição invisível
+    });
+    });
+});
+
+
 var btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event){
@@ -12,3 +33,13 @@ function auxToggleMenu(){
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+function exibeDescricao(){
+    const description = document.querySelector('.descricaoAreas');
+    description.style.display = 'block';
+}
+
+function ocultaDescricao(){
+    const description = document.querySelector('.descricaoAreas');
+    description.style.display = 'none';
+}
